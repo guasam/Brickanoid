3e7388568fca4e31b7013f702e9bb647d928926e
guasam <guasam@codesbiome.com>
2021-04-04 04:50:46 +0530

    Basic URP project setup

---
+++ Assets/Scripts/JustForFun.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class JustForFun : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Calling it just for fun");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
